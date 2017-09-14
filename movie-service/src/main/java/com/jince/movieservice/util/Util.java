1c83bc1e2059791592825e29761f69eb4123d08d
Jince Tom Varghese <jince.varghese@TVMATP6396L.local>
2017-09-15 01:21:06 +0530

    new changes for making micro services

---
+++ movie-service/src/main/java/com/jince/movieservice/util/Util.java
@@ -0,0 +1,16 @@
+package com.jince.movieservice.util;
+
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+
+public class Util {
+	public static String convertToJsonString(Object object) {
+
+		return convertToJsonObject(object).toString();
+	}
+
+	public static JsonObject convertToJsonObject(Object object) {
+
+		return new JsonObject(Json.encode(object));
+	}
+}
---
+++ user-service/src/main/java/com/jince/userservice/model/User.java
@@ -0,0 +1,57 @@
+package com.jince.userservice.model;
+
+import java.io.Serializable;
+
+import io.vertx.core.json.JsonObject;
+
+public class User implements Serializable {
+	public User(JsonObject json) {
+		this.name = json.getString("name");
+		this.email = json.getString("email");
+		this.id = json.getString("_id");
+	}
+
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+		this.id = "";
+	}
+
+	public User(String id, String name, String email) {
+		this.name = name;
+		this.email = email;
+		this.id = id;
+	}
+
+	private String name;
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public User setId(String id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public User setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public User setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	private String email;
+}
